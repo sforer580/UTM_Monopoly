4fa34894c5d49a3631dbec54fa14f54f5e90c02a
sforer580 <sforer580@gmail.com>
2016-05-31 11:08:45 -0700

    Initial Push
    First push to Github

---
+++ UTM_Monopoly/UTM_Monopoly/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  UTM_Monopoly
+//
+//  Created by Scott S Forer on 5/31/16.
+//  Copyright © 2016 Scott S Forer. All rights reserved.
+//
+
+#include <iostream>
+
+int main()
+{
+    return 0;
+}
